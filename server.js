69ce00e12bdd510f9394237a1e3fbdbb84d64b41
EdySmajler <edy.smajler@gmail.com>
2023-09-09 14:29:18 +0200

    another fix

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get('/scrape', async (req, res) => {
     // Return the scraping result to the frontend
     res.send(response);
   } catch (error) {
+    console.log("SOMETHING IS WRONG HERE?")
     console.error('Error occurred during scraping:', error);
     console.log(error)
     res.status(500).send('Error occurred during scraping');
